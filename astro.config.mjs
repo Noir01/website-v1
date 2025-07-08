9ae6e676d288e0c99ad49997b8cd8f4c3bbb002c
Aniruddh s <noirrr@duck.com>
2025-07-08 21:58:19 +0000

    Add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   site: 'https://portfolio.noir.ac',
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
 
   vite: {
     plugins: [tailwindcss()]
